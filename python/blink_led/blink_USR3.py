86430f5de39f5c22f9e0e11aab45151963e0fa20
Demo User <debian@beaglebone.localdomain>
2026-02-24 08:38:34 +0000

    ahhh

---
+++ python/blink_led/blink_USR3.py
@@ -0,0 +1,58 @@
+"""
+--------------------------------------------------------------------------
+License:   
+Copyright 2026 - Miller Harris
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+"""
+
+# Imports nescessary packages 
+import time
+import Adafruit_BBIO.GPIO as GPIO 
+
+
+def main():
+
+
+    GPIO.setup("USR3",GPIO.OUT) # Initiallizes for USR
+
+    try: # Runs indefinitely 
+        while True:
+            GPIO.output("USR3",GPIO.HIGH) # Turns on LED 
+            time.sleep(0.1) # Holds on for 0.1 seconds 
+            GPIO.output("USR3",GPIO.LOW) # Turns off LED
+            time.sleep(0.1) # Holds off for 0.1 seconds
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.output("USR3",GPIO.LOW)
+        GPIO.cleanup()
+if __name__ =="__main__":
+    main()
+        
